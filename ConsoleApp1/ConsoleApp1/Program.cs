464ff6913976605d63e60b6e43b28c93a0f7d3f8
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-01-27 20:26:43 +0300

    add clsCalculator

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -68,36 +68,111 @@ namespace ConsoleApp1
             return x2;
         }
     }
+}
+class clsCalculator
+{
+    private float _Result = 0;
+    private float _LastNumber = 0;
+    private string _LastOperation = "Clear";
 
-    internal class Program
+    private bool _IsZero(float Number)
     {
-        static void Main(string[] args)
-        {
-            //Create an object of Employee class.
-            clsA objA1 = new clsA();
-            clsA objA2 = new clsA();
-
-
-            objA1.x1 = 7;
-            objA2.x1 = 10;
-            //x2 is shared for all object because it's on the class level, you can access it 
-            //using the class name.
-            clsA.x2 = 100;
-
-
-            Console.WriteLine("objA1.x1:={0}", objA1.x1);
-            Console.WriteLine("objA2.x1:={0}", objA2.x1);
-            Console.WriteLine("objA1.method1 results:={0}", objA1.Method1());
-            Console.WriteLine("objA2.method1 results:={0}", objA2.Method1());
-
-
-            //Method 2 cannot be accessed through object, only through the class itself.
-            //  Console.WriteLine(objA1.Method2());
-            Console.WriteLine("static method2 results:={0}", clsA.Method2());
-
-            Console.WriteLine("static x2:={0}", clsA.x2);
-            Console.ReadLine();
-        }
+        return (Number == 0);
     }
 
+    public void Add(float Number)
+    {
+        _LastNumber = Number;
+        _LastOperation = "Adding";
+        _Result += Number;
+    }
+
+    public void Subtract(float Number)
+    {
+        _LastNumber = Number;
+        _LastOperation = "Subtracting";
+
+        _Result -= Number;
+    }
+
+    public bool Divide(float Number)
+    {
+
+        bool Succeeded = true;
+        _LastOperation = "Dividing";
+
+        if ( _IsZero(Number) )
+        {
+            _LastNumber = Number;
+            _Result /= 1;
+            Succeeded = false;
+        }
+        else
+        {
+            _LastNumber = Number;
+            _Result /= Number;
+        }
+
+        return Succeeded;
+    }
+
+    public void Multiply(float Number)
+    {
+        _LastNumber = Number;
+        _LastOperation = "Multiplying";
+        _Result *= Number;
+    }
+
+    public float GetFinalResults()
+    {
+        return _Result;
+    }
+
+    public void Clear()
+    {
+        _LastNumber = 0;
+        _LastOperation = "Clear";
+        _Result = 0;
+    }
+
+    public void PrintResult()
+    {
+        Console.WriteLine("Result After {0} {1} is : {2}" , _LastOperation , _LastNumber , _Result);
+    }
 }
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        clsCalculator Calculator1 = new clsCalculator();
+
+
+        Calculator1.Clear();
+
+        Calculator1.Add(10);
+        Calculator1.PrintResult();
+
+        Calculator1.Add(100);
+        Calculator1.PrintResult();
+
+        Calculator1.Subtract(20);
+        Calculator1.PrintResult();
+
+        Calculator1.Divide(0);
+        Calculator1.PrintResult();
+
+        Calculator1.Divide(2);
+        Calculator1.PrintResult();
+
+        Calculator1.Multiply(3);
+        Calculator1.PrintResult();
+
+        Calculator1.Clear();
+        Calculator1.PrintResult();
+
+        Console.ReadLine();
+
+    }
+}
+
