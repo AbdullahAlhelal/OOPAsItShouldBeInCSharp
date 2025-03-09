ec830b3c54d4a22a6fc8d9eea5b09b12471956db
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-09 20:12:12 +0300

    sendDatafrom_form1toform2

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/sendDatafrom_form1toform2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sendDatafrom_form1toform2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+            int _personid = -1;
+
+            if(int.TryParse(textBox1.Text,out _personid) && _personid != -1)
+            {
+                Form2 form2 = new Form2( _personid);
+                form2.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect input Plaese input number " , "Error" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/sendDatafrom_form1toform2/Form2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sendDatafrom_form1toform2
+{
+    public partial class Form2 : Form
+    {
+        private int _PersonId;
+        public Form2(int PersonId)
+        {
+            InitializeComponent();
+            _PersonId = PersonId;
+        }
+
+        private void Form2_Load(object sender , EventArgs e)
+        {
+            label2.Text = Convert.ToString(_PersonId);
+        }
+    }
+}
