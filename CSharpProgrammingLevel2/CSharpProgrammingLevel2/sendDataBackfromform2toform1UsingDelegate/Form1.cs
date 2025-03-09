96899751ff6aef9dc1cf2bdd3ef0693788649534
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-09 20:29:58 +0300

    add sendDataBackfromform2toform1UsingDelegate

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/sendDataBackfromform2toform1UsingDelegate/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sendDataBackfromform2toform1UsingDelegate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.dataBack += GetDataBack;
+            form.ShowDialog();
+        }
+
+        private void GetDataBack(object sender ,int id)
+        {
+            textBox1.Text = id.ToString();
+        }
+    }
+}
---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/sendDataBackfromform2toform1UsingDelegate/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sendDataBackfromform2toform1UsingDelegate
+{
+    public partial class Form2 : Form
+    {
+
+        public delegate void HandelDataBack(object sender , int id);
+
+
+        public  HandelDataBack dataBack;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+            int id = -1;
+            if (int.TryParse(textBox1.Text , out id))
+                dataBack?.Invoke(this , id);
+
+            this.Close();
+        }
+    }
+}
