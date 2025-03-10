96a96d8ff6d44b794090664720334e0b6e72f7f5
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-10 18:14:00 +0300

    use User Controle

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/UserControls/ctrlSimpleCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControls
+{
+    public partial class ctrlSimpleCalc : UserControl
+    {
+        public ctrlSimpleCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+            lblresult.Text = Convert.ToString( Convert.ToInt32( textBox1.Text) + Convert.ToInt32(textBox2.Text));
+        }
+    }
+}
