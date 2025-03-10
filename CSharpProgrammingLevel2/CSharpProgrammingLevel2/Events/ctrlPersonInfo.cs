5d516edaa27911840f5a58be641ba6f86ea00ced
Abdallh Alhelel <abdallh20hl@gmail.com>
2025-03-10 18:48:59 +0300

    Simple Event With Parameters

---
+++ CSharpProgrammingLevel2/CSharpProgrammingLevel2/Events/ctrlPersonInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events
+{
+    public partial class ctrlPersonInfo : UserControl
+    {
+
+        public event Action<string> OnPrint;
+
+        protected virtual void Printed(string FullName)
+        {
+           Action<string> action = OnPrint;
+            if (action != null)
+                OnPrint(FullName);
+
+        }
+        public ctrlPersonInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender , EventArgs e)
+        {
+            if ( OnPrint != null )
+                OnPrint(textBox1.Text + textBox2.Text);
+        }
+    }
+}
